c8559b6314b8a5099ef55aac15288e65099b011b
Keront <akill22077@gmail.com>
2026-04-07 15:37:34 +0300

    swiper ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.mainWithSlider__slider', {
         loop: true,
-        observer: true,           // Следить за изменениями
-        observeParents: true,     // Следить за родителями
+        // speed:500,
+        // Добавляем этот блок:
+        autoplay: {
+            delay: 5000, // Пауза между слайдами в миллисекундах (3 секунды)
+            disableOnInteraction: false, // Автоплей НЕ остановится, если вы кликните по слайдеру
+        },
+
+        observer: true,
+        observeParents: true,
         watchOverflow: true,
         pagination: {
             el: '.mainWithSlider__sliderZone .swiper-pagination',
